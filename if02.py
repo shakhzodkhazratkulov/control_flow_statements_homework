97e6a3e42fcddcbb340578d86f6c7a88516e080a
shakhzodkhazratkulov <shahzodxazratqulov9944@gmail.com>
2021-12-31 10:43:23 +0000

    2-misol

--- if02.py
+++ if02.py
@@ -6,4 +6,9 @@ def main(a):
     Returns:
         a: a increased by 1 if positive, else decreased by 2.
     """
-    return
+    if a > 0:
+        a += 1
+    if a < 0:
+        a -= 2
+    return a
+print(main(1))
